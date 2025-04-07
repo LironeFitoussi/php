5334ba7772152ba6b983a09d80584e070f6008a8
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-07 23:49:03 +0300

    L. 20

---
+++ Section 2 - php essentials/01-strings/strings.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+<body>
+    <pre><?php 
+        
+        $greeting =  'I\'m a string';
+        echo $greeting . '!!! ' . $greeting;
+        echo '<br />';
+        $name = "Lirone";
+        $subject = "PHP";
+
+        $text = 'I\'m ' . $name . ' and I\'m learning ' . $subject;
+        echo $text;
+    ?></pre>
+</body>
+</html>
