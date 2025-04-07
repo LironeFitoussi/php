f7962cc0bbb23b576faeb3c3f078cada6e423f5d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-07 18:29:26 +0300

    L. 15

---
+++ Section 1 - First Steps/05-restaurant/before/ingredients.php
@@ -0,0 +1,32 @@
+<?php
+include 'inc/header.inc.php';
+?>
+<h1>Our ingredients</h1>
+<p>
+  At Culinary Cove, our ethos is deeply rooted in the belief that
+  exceptional cuisine starts with exceptional ingredients. That's why we
+  are committed to using only natural, locally-sourced produce, free-range
+  meats, and sustainably caught seafood. Our culinary team works closely
+  with local farmers and artisans to ensure that the ingredients we use
+  are not only fresh but also ethically and sustainably produced. This
+  unwavering commitment to quality is evident in the vibrant flavors and
+  wholesome nutrition that characterize each dish on our menu.
+</p>
+<figure style="text-align: center">
+  <img
+    style="max-width: 20rem"
+    src="images/pexels-lisa-fotios-1126728.jpg"
+    alt="Our amazing restaurant" />
+  <figcaption>Our amazing restaurant</figcaption>
+</figure>
+<p>
+  In addition to providing world-class cuisine, we believe in creating a
+  welcoming and comfortable atmosphere where everyone can relax and enjoy.
+  Our restaurant serves as a haven for community, a place where people can
+  connect over a shared love for great food. We are committed to
+  excellence in every facet of our operation, from the food to the service
+  to the overall dining experience. At Culinary Cove, every meal is more
+  than just a dish; it's a journey into a world of unique and exciting
+  flavors.
+</p>
+<?php include 'inc/footer.inc.php'; ?>
