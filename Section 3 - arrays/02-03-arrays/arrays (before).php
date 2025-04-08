4cb5c730f859ccc137056234ac4bba447874b8e2
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-08 10:57:19 +0300

    L. 36

---
+++ Section 3 - arrays/02-03-arrays/arrays (before).php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <pre>
+        <?php
+            // $categories = array('Programming', 'Business', 'Art & Drawing', 'Self improvement', 'History', 5, 5 + 4);
+            $categories =['Programming', 'Business', 'Art & Drawing', 'Self improvement', 'History', 5, 5 + 4];
+            // echo $categories
+            var_dump($categories);
+            // var_dump($categories[0]);
+            echo $categories[0] . "\n";
+            echo $categories[3];
+            // echo "\n";
+
+            // $firstCategory = $categories[0];
+            // var_dump($firstCategory);
+        ?>
+    </pre>
+</body>
+
+</html>
