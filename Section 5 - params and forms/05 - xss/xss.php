8f713b6a84d8cbc27d171627f80d90e858ca4625
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-08 22:01:12 +0300

    L. 54

--- Section 5 - params and forms/05 - xss/xss.php
+++ Section 5 - params and forms/05 - xss/xss.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./simple.css" />
     <title>Document</title>
 </head>
-<body><pre><?php
-    var_dump($_POST);
-    function e($value) {
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-    }
-?></pre>
 
-<h1><?php if (!empty($_POST['firstname'])) echo e($_POST['firstname']); ?></h1>
+<body>
+    <pre><?php
+            var_dump($_POST);
+            function e($value)
+            {
+                return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+            }
+            ?></pre>
 
-<form action="xss.php" method="POST">
-    <input type="text" name="firstname" value="<?php if (!empty($_POST['firstname'])) echo e($_POST['firstname']); ?>" />
-    <input type="submit" value="Submit!" />
-</form>
-
-<h2><?php if (!empty($_GET['book'])) echo e($_GET['book']); ?></h2>
-
-<?php $course = 'PHP course'; ?>
-<p><?php echo e($course); ?></p>
-
+    <h1><?php if (!empty($_POST['firstname'])) echo e($_POST['firstname']); ?></h1>
+    <form action="xss.php" method="POST">
+        <input type="text" name="firstname" value="<?php if (!empty($_POST['firstname'])) echo htmlspecialchars($_POST['firstname'], ENT_QUOTES, 'UTF-8'); ?>" />
+        <input type="submit" value="Submit!" />
+    </form>
+    <h2><?php if (!empty($_GET['book'])) echo e($_GET['book']); ?></h2>
+    <?php
+    $course = 'PHP RUELS<script> alert("Hello")</script>';?>
+    <p><?php echo e($course)?></p>
 </body>
+
 </html>
