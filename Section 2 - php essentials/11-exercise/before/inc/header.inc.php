e813bd8abc446852ad341ab1e1516187fcb0b7cd
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-08 10:30:56 +0300

    L. 33

--- Section 2 - php essentials/11-exercise/before/inc/header.inc.php
+++ Section 2 - php essentials/11-exercise/before/inc/header.inc.php
@@ -11,15 +11,28 @@
 
 <body>
   <?php
-    empty($headerImg) && $headerImg = 'images/p'
+  empty($headerImg) && $headerImg = 'images/pexels-julia-volk-5273044.jpg'
   ?>
   <header class="header-with-background" style="background-image: url('<?php echo $headerImg; ?>'); ">
     <h1>Culinary Cove</h1>
     <p>Your sanctuary for exceptional flavors</p>
     <nav>
-      <a class="active" href="our-mission.php">Our mission</a>
-      <a href="ingredients.php">Ingredients</a>
-      <a href="menu.php">Menu</a>
+      <?php
+        if (!isset($pageKey)) $pageKey = '';
+      ?>
+      <a
+        <?php if ($pageKey === 'mission'): ?>
+        class="active"
+        <?php endif; ?>
+        href="our-mission.php">
+        Our mission
+      </a>
+      <a <?php if ($pageKey === 'ingredients') echo 'class="active"'; ?> href="ingredients.php">Ingredients</a>
+      <?php if ($pageKey === 'menu'): ?>
+        <a class="active" href="menu.php">Menu</a>
+      <?php else: ?>
+        <a href="menu.php">Menu</a>
+      <?php endif; ?>
     </nav>
   </header>
 
