86ee5f6a9790c88320edc25b09abecb8893d17d9
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-08 16:24:07 +0300

    L. 45

--- Section 4 - associative arrays/02 - associative arrays/books.php
+++ Section 4 - associative arrays/02 - associative arrays/books.php
@@ -1,52 +1,53 @@
  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./simple.css" />
-    <title>Document</title>
-</head>
-<body>
-<pre><?php
-/*
-$books = [
-    'Harry Potter',
-    'Lord of the Rings',
-    'The Little Prince',
-    'Don Quixote',
-    'Alice in Wonderland',
-];
+ <html lang="en">
 
-$authors = [
-    'J.K. Rowling',
-    'J.R.R. Tolkien',
-    'Antoine de Saint-Exupéry',
-    'Miguel de Cervantes',
-    'Lewis Carroll'
-];
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" type="text/css" href="./simple.css" />
+     <title>Document</title>
+ </head>
 
-var_dump("{$books[0]} has been written by {$authors[0]}.");
-*/
+ <body>
+     <pre><?php
+            // $books = [
+            //     'Harry Potter',
+            //     'Lord of the Rings',
+            //     'The Little Prince',
+            //     'Don Quixote',
+            //     'Alice in Wonderland',
+            // ];
 
-$books = [
-    'Harry Potter' => 'J.K. Rowling',
-    'Lord of the Rings' => 'J.R.R. Tolkien',
-    'The Little Prince' => 'Antoine de Saint-Exupéry',
-    'Don Quixote' => 'Miguel de Cervantes',
-    'Alice in Wonderland' => 'Lewis Carroll',
-];
+            // $authors = [
+            //     'J.K. Rowling',
+            //     'J.R.R. Tolkien',
+            //     'Antoine de Saint-Exupéry',
+            //     'Miguel de Cervantes',
+            //     'Lewis Carroll'
+            // ];
+            $books = [
+                'Harry Potter' => 'J.K. Rowling',
+                'Lord of the Rings' => 'J.R.R. Tolkien',
+                'The Little Prince' => 'Antoine de Saint-Exupéry',
+                'Don Quixote' => 'Miguel de Cervantes',
+                'Alice in Wonderland' => 'Lewis Carroll',
+            ];
 
-var_dump($books);
-var_dump(isset($books['Harry Potter']));
-var_dump(!empty($books['Harry Potter']));
-var_dump($books['Harry Potter']);
+            var_dump($books);
+            var_dump($books['Harry Potter']);
+            var_dump(isset($books['Harry Potter']));
+            var_dump(!empty($books['Harry Potter']));
+            
+            $key = 'Harry Potter';
+            var_dump($books[$key]);
+            var_dump($books['Harry' . ' ' . 'Potter']);
+            foreach ($books as $book => $author) {
+                echo "{$book} has been written by {$author}.\n";
+            }
 
-$key = 'Harry Potter';
-var_dump($books[$key]);
-var_dump($books['Harry' . ' ' . 'Potter']);
-
-?></pre>
+            ?></pre>
 
 
-</body>
-</html>
+ </body>
+
+ </html>
