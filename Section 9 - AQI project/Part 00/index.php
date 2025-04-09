89850c9158e1878333405556cb7d41ebe7204216
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 14:49:24 +0300

    L. 81

--- Section 9 - AQI project/Part 00/index.php
+++ Section 9 - AQI project/Part 00/index.php
@@ -1,10 +1,10 @@
 <?php
-/*
+
 $contents = file_get_contents(__DIR__ . '/../data/index.json');
 $data = json_decode($contents, true);
 var_dump($data);
 var_dump($data[0]['city']);
-*/
+
 
 // echo filesize('__DIR__ . '/../data/singapore.json.bz2');
 
