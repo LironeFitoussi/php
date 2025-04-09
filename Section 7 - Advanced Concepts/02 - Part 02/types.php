a1a744c34eca7868677f816aa5fe3e494c192fc6
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 10:29:39 +0300

    L. 66

--- Section 7 - Advanced Concepts/02 - Part 02/types.php
+++ Section 7 - Advanced Concepts/02 - Part 02/types.php
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./simple.css" />
     <title>Document</title>
 </head>
-<body><pre><?php
 
-if (isset($_GET['price'])) {
-    $price = $_GET['price'];
-    var_dump($price);
-}
-    
-?></pre></body>
+<body>
+    <pre><?php
+
+            if (isset($_GET['price']) && $_GET['price'] > 0) {
+                $price = (int) $_GET['price'];
+                var_dump($price);
+                var_dump($price * 1.19);
+            }
+
+
+            if (isset($_GET['name'])) {
+                $name = $_GET['name'];
+                if (is_array($name)) {
+                    foreach ($name as $persone) {
+                        var_dump($persone);
+                    }
+                }else {
+                    var_dump($name);
+                    // var_dump($name . "!!!");
+                }
+            }
+
+            ?></pre>
+            <a href="types.php?<?php echo http_build_query(['name' => ["Lirone", "Eden", "Eythan"]]) ?>">Get us</a>
+            <a href="types.php?<?php echo http_build_query(['name' => "Lirone"]) ?>">Get me</a>
+</body>
+
 </html>
+
+<!-- /types.php?price=50 -->
