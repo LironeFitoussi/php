ef94e0f2c3502d4cb69b1ce445e96ef984ac037b
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 15:21:33 +0300

    L. 83

---
+++ Section 9 - AQI project/Part 01/city.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/inc/functions.inc.php';
+$city = $_GET['city'];
+var_dump($city);
+?>
+
+<?php
+require __DIR__ . '/views/header.inc.php';
+?>
+
+<?php
+require __DIR__ . '/views/footer.inc.php';
+?>
--- Section 9 - AQI project/Part 01/index.php
+++ Section 9 - AQI project/Part 01/index.php
@@ -1,2 +1,25 @@
 <?php
-$contents - file_get_contents(__DIR__ . '/../index.josn');
+require __DIR__ . '/inc/functions.inc.php';
+
+$cities = json_decode(file_get_contents(__DIR__ . '/../data/index.json'), true);
+// var_dump($cities);
+?>
+
+<?php
+require __DIR__ . '/views/header.inc.php';
+?>
+
+<ul>
+    <?php foreach ($cities as $city): ?>
+        <li>
+            <a href="city.php?<?= http_build_query(['city' => $city['city']])?>">
+                <?= e($city['flag']) ?>
+                <?= e($city['city']) ?>,
+                <?= e($city['country']) ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<?php
+require __DIR__ . '/views/footer.inc.php';
+?>
