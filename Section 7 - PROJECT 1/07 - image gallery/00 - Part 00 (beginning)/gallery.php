f2ff82397a6c4d9168c3b3665f3b65f9f5023010
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 08:50:23 +0300

    L. 58

--- Section 7 - PROJECT 1/07 - image gallery/00 - Part 00 (beginning)/gallery.php
+++ Section 7 - PROJECT 1/07 - image gallery/00 - Part 00 (beginning)/gallery.php
@@ -5,5 +5,13 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<?php var_dump($imageTitles); ?>
+
+<?php foreach ($imageTitles as $key => $value): ?>
+    <figure>
+        <figcaption><?php echo $value?></figcaption>
+        <img src="<?php echo $key?>" alt="<?php echo $value?>">
+    </figure>
+<?php endforeach ?>
 
 <?php include './views/footer.php'; ?>
