0a6e09386b1ef49a65e96ebba910f40fbb9a08e6
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 08:38:17 +0300

    L. 57

--- Section 5 - params and forms/12 - include/before/include.php
+++ Section 5 - params and forms/12 - include/before/include.php
@@ -10,33 +10,33 @@
 
 <body>
     <?php
-    $currentPage = $_GET['page'];
-    function e($value)
-    {
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-    }
-
-    ?>
-
-    <form method="GET" action="include.php">
-        <select name="page">
-            <option value="">Please select a recipe</option>
-            <option value="citrus_salmon" <?php if (!empty($_GET['page']) && $_GET['page'] == 'citrus_salmon') echo 'selected'; ?>>Citrus Symphony Salmon</option>
-            <option value="mediterranian_pasta" <?php if (!empty($_GET['page']) && $_GET['page'] == 'mediterranian_pasta') echo 'selected'; ?>>Mediterranean Marvel Pasta</option>
-            <option value="sunset_risotto" <?php if (!empty($_GET['page']) && $_GET['page'] == 'sunset_risotto') echo 'selected'; ?>>Sunset Risotto</option>
-            <option value="tropical_tacos" <?php if (!empty($_GET['page']) && $_GET['page'] == 'tropical_tacos') echo 'selected'; ?>>Tropical Tango Tacos</option>
-        </select>
-        <input type="submit" value="Submit!" />
-    </form>
-
-    <?php
+        $currentPage = $_GET['page'];
+        function e($value) {
+            return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+        };
+        
         $pages = [
             'citrus_salmon' => "Citrus Symphony Salmon",
             'mediterranian_pasta' => "Mediterranean Marvel Pasta",
             'sunset_risotto' => "Sunset Risotto",
             'tropical_tacos' => "Tropical Tango Tacos"
         ];
-        if (!empty($currentPage) && in_array($currentPage, array_values($pages))) {
+    ?>
+
+    <form method="GET" action="include.php">
+        <select name="page">
+            <option value="">Please select a recipe</option>
+            <?php foreach ($pages as $link => $page): ?>
+                <option value="<?php echo e($link) ?>" <?php if (!empty($_GET['page']) && $_GET['page'] === $link) echo 'selected'; ?>>
+                    <?php echo $page ?>
+                </option>
+            <?php endforeach?>
+        </select>
+        <input type="submit" value="Submit!" />
+    </form>
+
+    <?php
+        if (!empty($currentPage) && in_array($currentPage, array_keys($pages))) {
             echo file_get_contents("pages/{$_GET['page']}.html");
         };
     ?>
