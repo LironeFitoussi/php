3743dfe3343b8acac117bc80f2bca0aa71abbfe0
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 09:45:07 +0300

    L. 62

--- Section 7 - PROJECT 1/07 - image gallery/00 - Part 00 (beginning)/gallery.php
+++ Section 7 - PROJECT 1/07 - image gallery/00 - Part 00 (beginning)/gallery.php
@@ -2,13 +2,16 @@
 include './inc/functions.inc.php';
 include './inc/images.inc.php';
 ?>
+
 <?php include './views/header.php'; ?>
 
-<?php foreach ($imageTitles as $key => $value): ?>
-    <a href="image.php?<?php echo http_build_query(["img" => $key]) ?>">        
-        <h3><?php echo e($value) ?></h3>
-        <img src="<?php echo rawurldecode("./images/{$key}") ?>" alt="<?php echo $value ?>">
-    </a>
-<?php endforeach ?>
+<div class="gallery-container">
+    <?php foreach ($imageTitles as $key => $value): ?>
+        <a href="image.php?<?php echo http_build_query(["img" => $key]) ?>" class="gallery-item">
+            <h3><?php echo e($value) ?></h3>
+            <img src="<?php echo rawurldecode("./images/{$key}") ?>" alt="<?php echo $value ?>">
+        </a>
+    <?php endforeach ?>
+</div>
 
 <?php include './views/footer.php'; ?>
