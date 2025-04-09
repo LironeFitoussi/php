99332d30e4ffbff530600f97431585979e99adf0
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 15:37:12 +0300

    L. 84

--- Section 9 - AQI project/Part 01/city.php
+++ Section 9 - AQI project/Part 01/city.php
@@ -1,13 +1,37 @@
 <?php
 require __DIR__ . '/inc/functions.inc.php';
-$city = $_GET['city'];
-var_dump($city);
+$city = null;
+if (!empty($_GET['city'])) $city = $_GET['city'];
+
+$filename = null;
+if (!empty($city)) {
+    $cities = json_decode(file_get_contents(__DIR__ . '/../data/index.json'), true);
+    foreach ($cities as $cityData) {
+        if (in_array($city, $cityData)) {
+            $filename = $cityData['filename'];
+            break;
+        }
+    };
+};
+
+if ($filename) {
+    // echo "<p>Data for the city <strong>{$city}</strong> has been found.</p>";
+    $data = json_decode(file_get_contents('compress.bzip2://' . __DIR__ . "/../data/{$filename}"), true)["results"];
+    var_dump($data);
+};
+
+
 ?>
 
 <?php
 require __DIR__ . '/views/header.inc.php';
 ?>
 
+<?php if (empty($city)): ?>
+    <p>The city you are looking for does not exist.</p>
+<?php else: ?>
+
+<?php endif ?>
 <?php
 require __DIR__ . '/views/footer.inc.php';
 ?>
