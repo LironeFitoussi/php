191eb2cfda6fcc5ccf6c570aef9fd33c990af0b2
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-09 10:54:44 +0300

    L. 68

--- Section 7 - Advanced Concepts/03 - Part 03.07/null-coalescing.php
+++ Section 7 - Advanced Concepts/03 - Part 03.07/null-coalescing.php
@@ -8,8 +8,17 @@
 </head>
 <body><pre><?php
 
-$name = @(string) ($_GET['name'] ?? '');
-var_dump($name);
+/**
+ * Retrieves the 'name' parameter from the GET request, or defaults to an empty string if not set.
+ * 
+ * - The null coalescing operator (??) is used to check if the 'name' key exists in the $_GET array.
+ * - If the 'name' key is not set, an empty string is returned as the default value.
+ * - The @(string) casting suppresses any potential warnings or errors during the type casting.
+ * 
+ * @var string $name The sanitized name parameter from the GET request or an empty string.
+ */
+$name = @(string) ($_GET['name'] ?? ''); 
+// var_dump($name);
     
 ?></pre>
 <a href="null-coalescing.php?<?php echo http_build_query(['name' => ['Jannis', 'Olivia']]); ?>">Link</a>
