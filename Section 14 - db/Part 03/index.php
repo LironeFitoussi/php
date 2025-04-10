aa58db7c1f595203027afc5a9bfde106ac842dcb
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-10 16:52:30 +0300

    L. 115

--- Section 14 - db/Part 03/index.php
+++ Section 14 - db/Part 03/index.php
@@ -1,6 +1,8 @@
 <?php
 
-
+function e($string) {
+    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+}
 function query($query) {
     $pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock', 'root', 'baba1234', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -9,7 +11,18 @@ function query($query) {
     $stmt = $pdo->prepare($query);
     $stmt->execute();
     
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-var_dump(query('SELECT * FROM `notes123`'));
+$results = query('SELECT * FROM `notes`');
+
+?>
+
+
+<ul>
+    <?php foreach($results as $res): ?>
+        <li>
+            <?= e($res['title']);?>
+        </li>
+    <?php endforeach ?>
+</ul>
