b8b35b6a5c8a74a9c777b7af8dfc50dce9ce326f
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-10 09:55:42 +0300

    L. 97

--- Section 11 - files/02 - Part 02/index.php
+++ Section 11 - files/02 - Part 02/index.php
@@ -1,40 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./styles/simple.css" />
     <title>Document</title>
 </head>
+
 <body>
-    <header><h1>Automatic Image List</h1></header>
-    <main><pre><?php 
-        $handle = opendir(__DIR__ . '/images');
-        /*
-        $currentFile = readdir($handle);
-        while ($currentFile !== false) {
-            var_dump($currentFile);
-            $currentFile = readdir($handle);
-        }
-        */
-        $images = [];
-        while (($currentFile = readdir($handle)) !== false) {
-            if ($currentFile === '.' || $currentFile === '..') {
-                continue;
-            }
-            var_dump($currentFile);
-            $images[] = $currentFile;
-        }
+    <header>
+        <h1>Automatic Image List</h1>
+    </header>
+    <main>
+        <pre><?php
+                // var_dump(pathinfo('index.php')['extension']);
+                // var_dump(pathinfo('index.php', PATHINFO_EXTENSION]);
 
-        // var_dump($str2 = $str = 'hello');
+                $handle = opendir(__DIR__ . '/images');
+                $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                $images = [];
+                while (($currentFile = readdir($handle)) !== false) {
+                    $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
+                    if ($currentFile === '.' ||
+                        $currentFile === '..' ||
+                        !in_array($extension, $allowedExtensions)
+                    ) continue;
+                    var_dump($currentFile);
+                    $images[] = $currentFile;
+                }
 
-        closedir($handle);
-    ?></pre>
+                closedir($handle);
+                ?></pre>
 
-        <?php foreach($images AS $image): ?>
+        <?php foreach ($images as $image): ?>
             <img src="images/<?php echo rawurlencode($image); ?>" alt="" />
         <?php endforeach; ?>
 
     </main>
 </body>
+
 </html>
