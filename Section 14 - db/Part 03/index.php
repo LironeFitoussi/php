7c6593a021691301ff053d9b4518157bb993a0e3
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-10 16:40:59 +0300

    L. 114

--- Section 14 - db/Part 03/index.php
+++ Section 14 - db/Part 03/index.php
@@ -1,6 +1,15 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock', 'root', 'baba1234', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
 
-var_dump($pdo);
+
+function query($query) {
+    $pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock', 'root', 'baba1234', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    
+    return $stmt->fetchAll();
+}
+
+var_dump(query('SELECT * FROM `notes123`'));
