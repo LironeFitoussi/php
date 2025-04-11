194a6f8a225cac3dbce916880515d000ddd5c56e
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-11 08:25:07 +0300

    L. 116-120

---
+++ Section 14 - db/Part 03/cmd.sql
@@ -0,0 +1,2 @@
+-- SELECT * FROM `notes` WHERE `title` = "Challenges Faced" ORDER BY `title` DESC;
+SELECT * FROM `notes` WHERE `id` = 5 OR `id` = 6 ORDER BY `title` DESC;
--- Section 14 - db/Part 03/index.php
+++ Section 14 - db/Part 03/index.php
@@ -1,28 +1,36 @@
 <?php
-
-function e($string) {
+function e($string)
+{
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
-function query($query) {
+function query($query, $binder)
+{
     $pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock', 'root', 'baba1234', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
     $stmt = $pdo->prepare($query);
+    foreach ($binder as $key => $value) {
+        $stmt->bindValue($key, $value);
+    }
     $stmt->execute();
-    
+
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-
-$results = query('SELECT * FROM `notes`');
-
+$title = "NEW Title";
+$content = "NEW Content";
+$id = $_GET["id"];
+// $results = query("SELECT * FROM `notes` WHERE `id` = :id", ["id" => $_GET["id"]]);
+// $results = query("INSERT INTO `notes` (`title`,`content`) VALUES (:title, :content)", ["title" => $title, "content" => $content]);
+// $results = query("UPDATE `notes` SET `title` = :title, `content` = :content WHERE `id` = :id", ["title" => $title, "content" => $content, "id" => $id]);
+$results = query("DELETE FROM `notes` WHERE `id` = :id", ["id" => $id]);
 ?>
 
 
-<ul>
-    <?php foreach($results as $res): ?>
+<!-- <ul>
+    <?php foreach ($results as $res): ?>
         <li>
-            <?= e($res['title']);?>
+            <?= e($res['title']); ?>
         </li>
     <?php endforeach ?>
-</ul>
+</ul> -->
