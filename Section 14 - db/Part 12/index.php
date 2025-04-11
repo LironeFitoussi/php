d1e81e97c5a3910279fb2e81c564ff312fc31572
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-11 08:49:10 +0300

    L. 122

--- Section 14 - db/Part 12/index.php
+++ Section 14 - db/Part 12/index.php
@@ -5,18 +5,31 @@ function e($value)
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock', 'root', 'fds f sdf ', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-} catch (PDOException $e) {
-    //throw $th;
-    echo "A database error occurred. Please try again later. \n ";
-    var_dump($e->getMessage());
-    die();
+function query($query, $binders)
+{
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=note_app;unix_socket=/tmp/mysql.sock;charset=utf8mb4', 'root', 'baba1234', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $stmt = $pdo->prepare($query);
+        foreach ($binders as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "A database error occurred. Please try again later. \n";
+        var_dump($e->getMessage());
+        die();
+    }
 }
+$title = "✅";
+$content = "✅";
+// $id = $_GET["id"];
 
-
+$results = query("INSERT INTO `notes` (`title`,`content`) VALUES (:title, :content)", ["title" => $title, "content" => $content]);
 
 /*
 $stmt = $pdo->prepare('UPDATE `notes` SET `title` = :title, `content` = :content WHERE `id` = :id');
