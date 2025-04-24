0fa6f7ba2fcd6a17b6f3260c30d18f81a7739df5
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-24 06:31:32 +0300

    L. 249: Done

--- Section 21- regex/02-regex.php
+++ Section 21- regex/02-regex.php
@@ -4,22 +4,26 @@ header('Content-Type: text/plain');
 
 $message = 'Happy 30th Birthday 20 ab!';
 
-$findings = null;
+$findings = [];
+// "/\d/" is equivalent to "/[0-9]/"
 var_dump(preg_match('/\d\d/', $message, $findings));
 var_dump($findings);
 
-var_dump(preg_match('/\D/', $message, $findings));
+// "/\D/" is equivalent to "/[^0-9]/"
 var_dump($findings);
 
+// "/\w/" is equivalent to "/[a-zA-Z0-9_]/"
 var_dump(preg_match('/\w/', $message, $findings));
 var_dump($findings);
-/*
-var_dump(preg_match_all('/\d\d/', $message, $findings));
-var_dump($findings);
-*/
 
-var_dump(preg_match('/\d\d\w\w/', $message, $findings));
+
+// var_dump(preg_match_all('/\d\d/', $message, $findings));
+// var_dump($findings);
+
+// /\d\d\s\w\w/ is equivalent to "/[0-9][0-9] [a-zA-Z][a-zA-Z]/"
+var_dump(preg_match_all('/\d\d\s\w\w/', $message, $findings));
 var_dump($findings);
 
-var_dump(preg_match('/\d\dth/', $message, $findings));
+// /\d\dth/ is equivalent to "/[0-9][0-9]th/"
+var_dump(preg_match_all('/\d\dth/', $message, $findings));
 var_dump($findings);
